7531f032df4e1c41c91e319bc139f8ba6e9571b9
ThienBao <nltb12042003@gmail.com>
2024-04-24 21:13:27 +0700

    update exr

---
+++ Goto and Setjmp/Ex1/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <setjmp.h>
+
+jmp_buf buf;
+int exception_code;
+char *error_code;
+
+#define TRY if ((exception_code = setjmp(buf)) == 0)
+#define CATCH(x) else if (exception_code == (x)) printf("%s\n", error_code);
+#define THROW(x, ERROR_CODE) {error_code = #ERROR_CODE; longjmp(buf, (x));}
+
+double divide(int a, int b) {
+    if (b == 0) 
+    {
+        THROW (1, ERROR: Divide by 0!!!);
+    } 
+    if (a * b < 0) THROW(2, ERROR: Another error has occured!!!);  
+    return (double) a / b;
+}
+
+int main() {
+    int a = 36; int b = 17;
+    TRY printf("Result of %d / %d = %.2f\n", a, b, divide(a, b));
+    CATCH (exception_code); 
+    return 0;
+}
---
+++ Goto and Setjmp/README.md
@@ -0,0 +1,2 @@
+Exercise Link     
+https://docs.google.com/document/d/1xmmRqDHt0Ewai3tRQ-eVVE_VvHZ7kIF2GQvNv8Ayaps/edit
