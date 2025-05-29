828e880473d552821ca0d32f5300c259aa99ff59
skytold <nltb12042003@gmail.com>
2025-05-29 23:59:00 +0700

    continue with core module 1: init RoomManager

---
+++ C++_Project/Hotel-Management/Src/RoomManager.cpp
@@ -0,0 +1,10 @@
+#include "RoomManager.hpp"
+#include <fstream>
+#include <iostream>
+#include <limits>      // use for ignore wrong input
+
+using namespace std;
+
+RoomManager::RoomManager() {}
+
+RoomManager::~RoomManager() {}
