1be9e6f824c7f34972542b374bb1ea3830529152
skytold <nltb12042003@gmail.com>
2025-05-30 00:14:13 +0700

    continue with the frame of module 1 core: RoomManager

--- C++_Project/Hotel-Management/Src/RoomManager.cpp
+++ C++_Project/Hotel-Management/Src/RoomManager.cpp
@@ -8,3 +8,27 @@ using namespace std;
 RoomManager::RoomManager() {}
 
 RoomManager::~RoomManager() {}
+
+void RoomManager::loadFromFile(const string& filename) {}
+
+void RoomManager::saveToFile(const string& filename) const {}
+
+int RoomManager::findRoomIndex(int roomID) const {}
+
+bool RoomManager::isRoomExist(int roomID) const {}
+
+bool RoomManager::isRoomAvailable(int roomID) const {}
+
+void RoomManager::bookRoom(int roomID) {}
+
+void RoomManager::checkoutRoom(int roomID) {}
+
+void RoomManager::displayAllRooms() const {}
+
+void RoomManager::addRoom() {}
+
+void RoomManager::modifyRoom(int roomID) {}
+
+void RoomManager::deleteRoom(int roomID) {}
+
+void RoomManager::showMenu() {}
