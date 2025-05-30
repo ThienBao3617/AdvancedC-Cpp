27933dd960f051d60573709876e10b1fe337e0f8
skytold <nltb12042003@gmail.com>
2025-05-30 23:21:41 +0700

    continue building module 1 core

--- C++_Project/Hotel-Management/Src/RoomManager.cpp
+++ C++_Project/Hotel-Management/Src/RoomManager.cpp
@@ -1,7 +1,7 @@
 #include "RoomManager.hpp"
-#include <fstream>
+#include <fstream> // use for read file, write file
 #include <iostream>
-#include <limits>      // use for ignore wrong input
+#include <limits> // use for ignore wrong input
 
 using namespace std;
 
@@ -9,13 +9,51 @@ RoomManager::RoomManager() {}
 
 RoomManager::~RoomManager() {}
 
-void RoomManager::loadFromFile(const string& filename) {}
+void RoomManager::loadFromFile(const string& filename) {
+    rooms.clear();
+    ifstream inFile(filename);
+    if(!inFile.is_open()){
+        cerr << "Error: cannot open file " << filename << "\n";
+        return;
+    }
 
-void RoomManager::saveToFile(const string& filename) const {}
+    string line;
+    // remove header (if any)
+    getline(inFile, line);
+    while(getline(inFile, line)){
+        if(line.empty()) continue;
+        Room r = Room::fromCSV(line);
+        rooms.push_back(r);
+    }
+    inFile.close();
+}
 
-int RoomManager::findRoomIndex(int roomID) const {}
+void RoomManager::saveToFile(const string& filename) const {
+    ofstream outFile(filename);
+    if(!outFile.is_open()){
+        cerr << "Error: cannot write to file " << filename << "\n";
+        return;
+    }
+    // write header
+    outFile << "roomID,type,price,isBooked\n";
+    for(const auto& r : rooms){
+        outFile << r.toCSV() << "\n";
+    }
+    outFile.close();
+}
 
-bool RoomManager::isRoomExist(int roomID) const {}
+int RoomManager::findRoomIndex(int roomID) const {
+    for(size_t i = 0; i < rooms.size(); i++){
+        if(rooms[i].getID() == roomID){
+            return static_cast<int>(i);
+        }
+    }
+    return -1; // not found
+}
+
+bool RoomManager::isRoomExist(int roomID) const {
+    return findRoomIndex(roomID) != -1;
+}
 
 bool RoomManager::isRoomAvailable(int roomID) const {}
 
