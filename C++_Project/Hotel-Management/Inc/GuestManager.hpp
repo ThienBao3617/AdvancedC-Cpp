53718b2a662302e8215653b329ce942ed8b24a2e
skytold <nltb12042003@gmail.com>
2025-06-02 14:14:00 +0700

    create mod2 header: guestmgr

---
+++ C++_Project/Hotel-Management/Inc/GuestManager.hpp
@@ -0,0 +1,37 @@
+#ifndef GUESTMANAGER_HPP
+#define GUESTMANAGER_HPP
+
+using namespace std;
+
+#include <vector>
+#include <string>
+#include "Guest.hpp"
+#include "RoomManager.hpp" // to check the room's availability
+
+class GuestManager {
+private:
+	vector<Guest> guests;
+public:
+	GuestManager();
+	~GuestManager();
+
+	void loadFromFile(const string& filename);
+	void saveToFile(const string& filename) const;
+
+	void addGuest(RoomManager& roomMgr); // Require user to enter info and book room
+	void modifyGuest(int guestID) const; // Edit guest info (not including room changes)
+	void deleteGuest(int guestID); // Delete guest (but need to check if checked out yet)
+	void displayAllGuest() const;
+
+	int findGuestIndex(int guestID) const;
+	bool isGuestExist(int guestID) const;
+
+	void checkOutGuest(int guetsID, RoomManager& roomMgr); // when checkout, room will be set into "available"
+	
+	// Allows searching for guests by name/ID, etc.
+	void searchGuestByName(const string& name) const;
+
+	void showMenu(RoomManager& roomMgr); // separate menu for this module
+};
+
+#endif
